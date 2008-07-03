da2a253b08f7c2f46289c416e27fec05f5d7d063
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-03 01:22:48 -0700

    Add a new failing test.

--- Example/test/test_tame.rb
+++ Example/test/test_tame.rb
@@ -12,4 +12,9 @@ class TestTame < Test::Unit::TestCase
     tame = OSX::Tame.alloc.init;
     assert_equal(7, tame.add_to(3,4));
   end
+  
+  def test_tame_should_add_negative_numbers
+    tame = OSX::Tame.alloc.init
+    assert_equal(-16, tame.add_to(-10, -6))
+  end
 end
