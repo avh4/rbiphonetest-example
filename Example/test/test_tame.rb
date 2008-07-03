5b0b41a81da0b5ee9236a3bb880820f6233f1a9d
Aaron VonderHaar <gruen0aermel@gmail.com>
2008-07-03 01:17:11 -0700

    Added a broken test to test_tame.rb

--- Example/test/test_tame.rb
+++ Example/test/test_tame.rb
@@ -7,4 +7,9 @@ class TestTame < Test::Unit::TestCase
   def test_tame_class_exists
     OSX::Tame
   end
+  
+  def test_tame_should_add_numbers
+    tame = OSX::Tame.alloc.init;
+    assert_equal(7, tame.add_to(3,4));
+  end
 end
